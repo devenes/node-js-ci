de18abbd05f078a57d1693bd8a92e5d36b56c5ac
Enes <66560757+devenes@users.noreply.github.com>
2022-04-27 14:39:38 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/test", (_req, res) => {
-    res.status(200).send("Hello world")
-})
+    res.status(200).send("Hello Enes!")
+});
+
 module.exports = app;
